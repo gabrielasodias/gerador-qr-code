bbaba19cb9455a1f8aa095d9e21d4b3f7cbaf91b
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-04-30 12:59:37 -0300

    qr code generator

--- qr_code.py
+++ qr_code.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+import qrcode
+from PIL import ImageTk
+
+# Função para gerar o QR Code
+def qrcode_generator():
+    data_to_generate = textEntry.get() # Obtenção dos dados a serem gerados
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    ) # Criação do objeto QRCode
+
+    qr.add_data(data_to_generate) # Adicionando os dados ao QRCode
+    qr.make(fit=True)  # Gerando o QRCode
+    img = qr.make_image(fill_color="black", back_color="white") # Criação da imagem do QRCode com as cores configuradas
+    img.thumbnail((300, 300)) # Redimensionamento da imagem (tamanho máximo)
+   
+    # Configuração da imagem para exibição no widget
+    show_img = ImageTk.PhotoImage(img)
+    label_img.configure(image=show_img)
+    label_img.image = show_img
 
 # Configurações da janela principal
 window = tk.Tk()
@@ -15,8 +37,8 @@ label_text.grid(column=0, row=0, columnspan=2, pady=10,)
 textEntry = tk.Entry()
 textEntry.grid(column=0, row=1, columnspan=2, padx=10)
 
-# Botão para gerar QR Code
-button = tk.Button(window, text="Gerar QR Code",)
+# Botão para chamar a função que gera o QR Code
+button = tk.Button(window, text="Gerar QR Code", command=qrcode_generator)
 button.grid(column=0, row=2, columnspan=2, pady=10)
 
 # Label para exibir a imagem do QR Code
